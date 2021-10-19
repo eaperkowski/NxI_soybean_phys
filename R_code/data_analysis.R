6edf0563bf9414fea986615013b5fdbb1cccb0b6
eaperkowski <evan.a.perkowski@ttu.edu>
2021-10-19 14:53:46 -0500

    final run through of analyses, including Rd:Vcmax, Jmax:Vcmax, and Vcmax:gs

--- R_code/data_analysis.R
+++ R_code/data_analysis.R
@@ -15,15 +15,40 @@ emm_options(opt.digits = FALSE)
 data <- read.csv("../data/trait_data.csv",
                  na.strings = "NA")
 
-## Add Rd:Vcmax and Jmax25:Vcmax25 column
+## Add Rd:Vcmax, Jmax25:Vcmax25, Vcmax:gs column
 data$Rd.Vcmax <- data$Rd.TPU / data$Vcmax.TPU
 data$Jmax25.Vcmax25 <- data$Jmax25.TPU / data$Vcmax25.TPU
+data$Vcmax.gs <-  data$Vcmax.TPU / data$gsw
+
 head(data)
+
+##########################################################################
+## A400
+##########################################################################
+a400 <- lmer(A ~ n.trt * inoc + (1 | block),
+             data = data)
+
+# Check model assumptions
+plot(a400)
+qqnorm(residuals(a400))
+qqline(residuals(a400))
+hist(residuals(a400))
+shapiro.test(residuals(a400))
+outlierTest(a400)
+
+# Model output
+summary(a400)
+Anova(a400)
+r.squaredGLMM(a400)
+
+# Pairwise comparisons
+emmeans(a400, pairwise~n.trt)
 
 ##########################################################################
 ## Vcmax25 with TPU
 ##########################################################################
-vcmax25 <- lmer(Vcmax25.TPU ~ n.trt * inoc + (1 | block), data = data)
+vcmax25 <- lmer(Vcmax25.TPU ~ n.trt * inoc + (1 | block),
+                data = data)
 
 # Check model assumptions
 plot(vcmax25)
@@ -44,7 +69,8 @@ emmeans(vcmax25, pairwise~n.trt) # Nitrogen addition decreases Vcmax25??
 ##########################################################################
 ## Jmax25 with TPU
 ##########################################################################
-jmax25 <- lmer(Jmax25.TPU ~ n.trt * inoc + (1 | block), data = data)
+jmax25 <- lmer(Jmax25.TPU ~ n.trt * inoc + (1 | block),
+               data = data)
 
 # Check model assumptions
 plot(jmax25)
@@ -67,7 +93,8 @@ emmeans(jmax25, pairwise~n.trt) # Nitrogen addition decreases Jmax25?
 ##########################################################################
 data$Jmax25.Vcmax25[c(17, 62)] <- NA
 
-jmax25.vcmax25 <- lmer(log(Jmax25.Vcmax25) ~ n.trt * inoc + (1 | block), data = data)
+jmax25.vcmax25 <- lmer(log(Jmax25.Vcmax25) ~ n.trt * inoc + (1 | block),
+                       data = data)
 
 
 # Check model assumptions
@@ -93,7 +120,8 @@ cld(emmeans(jmax25.vcmax25, pairwise~n.trt*inoc))
 ##########################################################################
 data$Rd.TPU[c(2, 7, 31)] <- NA
 
-rd <- lmer(log(Rd.TPU) ~ n.trt * inoc + (1 | block), data = data)
+rd <- lmer(log(Rd.TPU) ~ n.trt * inoc + (1 | block),
+           data = data)
 
 # Check model assumptions
 plot(rd)
@@ -120,7 +148,8 @@ emmeans(rd, pairwise~n.trt*inoc)
 ##########################################################################
 data$Rd.Vcmax[c(7, 31)] <- NA
 
-rd.vcmax <- lmer(Rd.Vcmax ~ n.trt * inoc + (1 | block), data = data)
+rd.vcmax <- lmer(Rd.Vcmax ~ n.trt * inoc + (1 | block),
+                 data = data)
 
 # Check model assumptions
 plot(rd.vcmax)
@@ -139,3 +168,98 @@ r.squaredGLMM(rd.vcmax)
 emmeans(rd.vcmax, pairwise~n.trt)
 # High nitrogen increases Rd:Vcmax regardless of inoculation status
 # Driven by both an increase in Rd and reduction in Vcmax with increasing soil N
+
+##########################################################################
+## Gs
+##########################################################################
+gs <- lmer(gsw ~ n.trt * inoc + (1 | block),
+           data = data)
+
+# Check model assumptions
+plot(gs)
+qqnorm(residuals(gs))
+qqline(residuals(gs))
+hist(residuals(gs))
+shapiro.test(residuals(gs))
+outlierTest(gs)
+
+# Model output
+summary(gs)
+Anova(gs)
+r.squaredGLMM(gs)
+
+# Pairwise comparisons
+emmeans(gs, pairwise~n.trt)
+# Increasing nitrogen decreased stomatal conductance
+
+##########################################################################
+## Ci:Ca
+##########################################################################
+cica <- lmer(ci.ca ~ n.trt * inoc + (1 | block),
+             data = data)
+
+# Check model assumptions
+plot(cica)
+qqnorm(residuals(cica))
+qqline(residuals(cica))
+hist(residuals(cica))
+shapiro.test(residuals(cica))
+outlierTest(cica)
+
+# Model output
+summary(cica)
+Anova(cica)
+r.squaredGLMM(cica)
+
+# Pairwise comparisons
+emmeans(cica, pairwise~n.trt)
+# Increasing nitrogen decreases Ci:Ca
+
+##########################################################################
+## iWUE
+##########################################################################
+iwue <- lmer(log(iwue) ~ n.trt * inoc + (1 | block),
+             data = data)
+
+# Check model assumptions
+plot(iwue)
+qqnorm(residuals(iwue))
+qqline(residuals(iwue))
+hist(residuals(iwue))
+shapiro.test(residuals(iwue))
+outlierTest(iwue)
+
+# Model output
+summary(iwue)
+Anova(iwue)
+r.squaredGLMM(iwue)
+
+# Pairwise comparisons
+emmeans(iwue, pairwise~n.trt)
+# Increasing nitrogen increases iWUE
+
+##########################################################################
+## Vcmax:gs
+##########################################################################
+vcmax.gs <- lmer(log(Vcmax.gs) ~ n.trt * inoc + (1 | block), 
+                 data = data)
+
+# Check model assumptions
+plot(vcmax.gs)
+qqnorm(residuals(vcmax.gs))
+qqline(residuals(vcmax.gs))
+hist(residuals(vcmax.gs))
+shapiro.test(residuals(vcmax.gs))
+outlierTest(vcmax.gs)
+
+# Model output
+summary(vcmax.gs)
+Anova(vcmax.gs)
+r.squaredGLMM(vcmax.gs)
+
+# Pairwise comparisons
+emmeans(vcmax.gs, pairwise~n.trt)
+# Increasing nitrogen increases Vcmax:gs through a relatively
+# larger decrease in gs than Vcmax25
+
+
