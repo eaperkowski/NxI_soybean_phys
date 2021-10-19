369c1dda3e65359a9ace2cd4abcd3bf044147d43
eaperkowski <evan.a.perkowski@ttu.edu>
2021-10-19 10:34:36 -0500

    optimize leaf trait code, remove points that cause poor aci fit, write csv with trait data

--- R_code/create_leaftrait_file.R
+++ R_code/create_leaftrait_file.R
@@ -10,6 +10,12 @@ library(reshape)
 library(plantecophys)
 
 #####################################################################
+# Load .csv file with dry weight data
+#####################################################################
+#dry.wgt <- read.csv("../data/dry.wgt.csv")
+
+
+#####################################################################
 # Determine leaf areas
 #####################################################################
 ## Path for leaf area and image J software
@@ -27,6 +33,7 @@ leaf.area <- run.ij(path.imagej = imagej.localaddress,
 ## Then, remove R from rep name a add a leading zero for all single 
 ## digit reps
 leaf.area <- leaf.area %>%
+  #full_join(dry.wgt) %>%
   separate(col = "sample", 
            sep = "(_*)[_]_*",
            into = c("rep", "n.trt", "inoc"),
@@ -38,6 +45,8 @@ leaf.area <- leaf.area %>%
                 id = sample) %>%
   arrange(rep) %>%
   mutate(block = rep(1:4, each = 16))
+         #sla = dry.wgt / focal.area,
+         #narea = n.leaf / sla)
 
 #####################################################################
 # Determine respiration values - to later be merged to A/Ci files
@@ -56,9 +65,11 @@ df.resp <- lapply(file.list, read.csv)
 resp.merged <- df.resp %>%
   merge_all() %>%
   group_by(id) %>%
-  summarize(resp = mean(A, na.rm = TRUE)) %>%
-  mutate(resp = abs(resp))
-head(resp.merged)
+  dplyr::select(id, A) %>%
+  mutate(resp = abs(A)) %>%
+  summarize(resp = mean(resp, na.rm = TRUE)) %>%
+  data.frame()
+resp.merged
 
 #####################################################################
 # Load A/Ci curves, put in central data frame, add respiration means,
@@ -72,13 +83,16 @@ file.list <- setNames(file.list, file.list)
 df.aci <- lapply(file.list, read.csv)
 
 ## Subset A/Ci curve to only columns necessary for 'fitaci'
-## function. Add respiration mean values to each ID
+## function. Add respiration mean values to each ID. Also,
+## add keep row column and insert "no" when 400 ppm CO2 measurement
+## is recorded after a 2000 ppm CO2 measurement. This is to later remove
+## these values in the curve estimation
 aci.merged <- df.aci %>%
   merge_all() %>%
   group_by(id) %>%
-  select(id, A, Ci, gsw, 
+  dplyr::select(id, machine, A, Ci, gsw, 
          CO2_s,	CO2_r,	H2O_s,	H2O_r,
-         Qin, VPDleaf, Flow,	Tair,	Tleaf) %>%
+         Qin, VPDleaf, Flow,	Tair,	TleafEB) %>%
   arrange(id) %>%
   left_join(resp.merged, by = "id") %>%
   separate(col = "id",
@@ -88,24 +102,38 @@ aci.merged <- df.aci %>%
   mutate(rep = gsub("r", "", rep),
          rep = str_pad(rep, width = 2, side = "left", pad = "0"),
          n.trt = toupper(n.trt),
-         inoc = toupper(inoc)) %>%
+         inoc = toupper(inoc),
+         keep.row = ifelse(lag(CO2_r > 1501, n = 1L),"no","yes"),
+         keep.row = tidyr::replace_na(keep.row, "yes")) %>%
   data.frame()
+aci.merged
 
-head(aci.merged)
+## Remove rows based on A/Ci fits
+aci.merged$keep.row[c(31, 38, 90, 97, 148, 155, 163, 170, 214,
+                      222, 229, 267, 274, 276, 282, 408, 401,
+                      431, 444, 453, 446, 468, 506, 513, 519,
+                      526, 698, 705, 749, 755, 802, 809, 815,
+                      871, 872, 922, 929, 937, 938)] <- "no"
 
+aci.merged
 #####################################################################
 # Run A/Ci curves with TPU limitation
 #####################################################################
-aci.tpu <- fitacis(aci.merged, 
-                   group = "id",
-                   varnames = list(ALEAF = "A",
-                                   Tleaf = "Tleaf",
-                                   Ci = "Ci",
-                                   PPFD = "Qin",
-                                   Rd = "resp"),
-                   fitTPU = TRUE,
-                   useRd = TRUE,
-                   Tcorrect = FALSE)
+aci.tpu <- aci.merged %>%
+  filter(keep.row == "yes") %>%
+  fitacis(group = "id",
+          varnames = list(ALEAF = "A",
+                          Tleaf = "TleafEB",
+                          Ci = "Ci",
+                          PPFD = "Qin",
+                          Rd = "resp"),
+          fitTPU = TRUE,
+          useRd = TRUE,
+          Tcorrect = FALSE)
+
+## Check plots for fit and check if any points need to be removed
+# plot(aci.tpu[[4]])
+# aci.tpu[[4]]
 
 ## Remove r4_hn_ni from list (no fit because no Rd value)
 aci.tpu$r4_hn_ni <- NULL
@@ -119,32 +147,120 @@ r4_hn_ni <- fitaci(subset(aci.merged, id == "r4_hn_ni"),
                    fitTPU = TRUE,
                    useRd = FALSE,
                    Tcorrect = FALSE)
+
+## Check model fit for r4_hn_ni
 plot(r4_hn_ni)
+
+## Create data frame by transposing r4_hn_ni coefficients, to be
+## merged back into larger fitacis list
 aci.r4_hn_ni <- data.frame(id = "r4_hn_ni",
                            t(coef(r4_hn_ni)))
+
+## Check that transposing looks right. Should have one column with id
+## on left column with column for each of Vcmax, Jmax, Rd, and TPU
 head(aci.r4_hn_ni)
 
 ## Extract coefficients and separate id into rep, n.trt, and inoc.
-## Also, merge separate r4_hn_ni coefficients
+## Also, merge r4_hn_ni coefficients
 aci.coef <- aci.tpu %>%
   coef() %>%
-  select(id, Vcmax, Jmax, Rd, TPU) %>%
   full_join(aci.r4_hn_ni) %>%
+  dplyr::select(id, 
+         Vcmax.TPU = Vcmax, 
+         Jmax.TPU = Jmax, 
+         Rd.TPU = Rd, 
+         TPU.TPU = TPU) %>%
   separate(col = "id",
-           sep = "(_*)[_]_*",
-           into = c("rep", "n.trt", "inoc"),
-           remove = FALSE) %>%
+         sep = "(_*)[_]_*",
+         into = c("rep", "n.trt", "inoc"),
+         remove = FALSE) %>%
   mutate(rep = gsub("r", "", rep),
          rep = str_pad(rep, width = 2, side = "left", pad = "0"),
          n.trt = toupper(n.trt),
          inoc = toupper(inoc)) %>%
   arrange(rep) %>%
   mutate(block = rep(1:4, each = 16)) %>%
-  data.frame() %>%
-  left_join(leaf.area)
+  data.frame()
 
-aci.coef
+## Create data frame with id and machine, join with aci.coef file
+aci.coef <- aci.merged %>%
+  group_by(id) %>%
+  summarize(machine = unique(machine)) %>%
+  left_join(aci.coef) %>%
+  dplyr::select(id, rep, n.trt, inoc, block, machine, everything()) %>%
+  data.frame()
 
+## Check aci.coef data frame. Should have id, n.trt, inoc, machine, 
+## model coefficients, and block as designated columns
+head(aci.coef)
+
+#####################################################################
+# Run A/Ci curves without TPU limitation
+#####################################################################
+aci.notpu <- aci.merged %>%
+  filter(keep.row == "yes") %>%
+  fitacis(group = "id",
+          varnames = list(ALEAF = "A",
+                          Tleaf = "TleafEB",
+                          Ci = "Ci",
+                          PPFD = "Qin",
+                          Rd = "resp"),
+          fitTPU = FALSE,
+          useRd = TRUE,
+          Tcorrect = FALSE)
+
+## Remove r4_hn_ni from list (no fit because no Rd value)
+aci.notpu$r4_hn_ni <- NULL
+
+## Do fitaci fxn for r4_hn_ni with useRd = FALSE
+r4_hn_ni.notpu <- fitaci(subset(aci.merged, id == "r4_hn_ni"),
+                   varnames = list(ALEAF = "A",
+                                   Tleaf = "TleafEB",
+                                   Ci = "Ci",
+                                   PPFD = "Qin"),
+                   fitTPU = FALSE,
+                   useRd = FALSE,
+                   Tcorrect = FALSE)
+
+## Check model fit for r4_hn_ni
+plot(r4_hn_ni.notpu)
+
+## Create data frame by transposing r4_hn_ni coefficients, to be
+## merged back into larger fitacis list
+aci.r4_hn_ni.notpu <- data.frame(id = "r4_hn_ni",
+                           t(coef(r4_hn_ni.notpu)))
+
+## Check that transposing looks right. Should have one column with id
+## on left column with column for each of Vcmax, Jmax, Rd, and TPU
+head(aci.r4_hn_ni.notpu)
+
+## Extract coefficients and separate id into rep, n.trt, and inoc.
+## Also, merge r4_hn_ni coefficients
+aci.coef <- aci.notpu %>%
+  coef() %>%
+  full_join(aci.r4_hn_ni.notpu) %>%
+  full_join(aci.coef) %>%
+  dplyr::select(id, rep, n.trt,
+                inoc, block, machine,
+                Vcmax.noTPU = Vcmax, 
+                Jmax.noTPU = Jmax, 
+                Rd,
+                Vcmax.TPU,
+                Jmax.TPU,
+                TPU.TPU) %>%
+  mutate(Rd.Vcmax.noTPU = Rd / Vcmax.noTPU,
+         Rd.Vcmax.TPU = Rd / Vcmax.TPU) %>%
+  full_join(leaf.area) 
+
+## Check aci.coef data frame. Should have coefficients with and without
+## TPU fit. Rd is the same across all fits, so only including single Rd
+## value. Includes Rd:Vcmax with and without TPU and focal leaf area. 
+## Should also include concatenated id, and columns for rep, n.trt, inoc,
+## block number, and machine
+head(aci.coef)
+
+
+## Write .csv file for leaf trait data
 write.csv(aci.coef, "../data/trait_data.csv")
 
 
